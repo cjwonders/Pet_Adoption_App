8f4b90c72ea5736d15e6dd8446739b0182ecb37f
Chirag Jadhwani <chirag@chiragjadhwani.com>
2020-05-25 10:18:47 +0530

    Initial commit

---
+++ app/Http/Controllers/AppController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Pets;
+
+class AppController extends Controller
+{
+    public function index () {
+        return view('index');
+    }
+
+    public function cats () {
+        return view('cats');
+    }
+
+    public function dogs () {
+        return view('dogs');
+    }
+
+}
