87f46abe691cd28a83c9ba81362f6e84297044de
Chirag Jadhwani <chirag@chiragjadhwani.com>
2020-05-25 10:26:20 +0530

    final notes

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,24 @@
 |
 */
 
+/*
+
+Hey!! Thanks for reviewing my project :D
+
+Here's how I acheived all the criterias:
+
+1. CSS, JS, and favicon used. Files in the resources folder.
+
+2. Routes created! One controller used for routing which has three functions returning the correct view. The controller is AppController.php file.
+
+3. The three views created. 
+
+4. Head, nav and footer created.
+
+5. They use @include :D
+
+*/
+
 Route::get('/', 'AppController@index');
 
 Route::get('/dogs', 'AppController@dogs');
